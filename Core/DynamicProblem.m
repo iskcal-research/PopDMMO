6d7b5f1954268b6494e6fbee82320686689fd6d2
iskcal-research <53363976+iskcal-research@users.noreply.github.com>
2022-01-19 15:23:45 +0800

    update

--- Core/DynamicProblem.m
+++ Core/DynamicProblem.m
@@ -2,11 +2,13 @@ classdef (Abstract)DynamicProblem < Problem
     properties(SetAccess = protected)
         proRand;        % used for the dynamic random change
         env;            % the current environment number;
+        maxEnv;         % the maximum environment number;
     end
     methods
         function obj = DynamicProblem()
             obj.proRand = RandStream.create('mt19937ar','seed', 0);
             obj.env = 0;
+            obj.maxEnv = 60;
         end
         
         function ChangeDynamic(obj)
